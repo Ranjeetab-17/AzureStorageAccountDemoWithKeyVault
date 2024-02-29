2d988af8f4ebb90b9ae562a32b4152cee9b08225
Ranjeet <ranjeetab17@gmail.com>
2024-02-29 20:05:19 +0530

    Add project files.

---
+++ Controllers/BlobsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureStorageAccountDemo.Controllers
+{
+    public class BlobsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public BlobsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> filelist = new List<string>();
+
+            // string blobstorageconnectionstring = _configuration.GetConnectionString("p2pdemoblobconn");
+
+            string blobstorageconnectionstring = _configuration.GetValue<string>("p2pdemoblobconn");
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(blobstorageconnectionstring, "photos");
+
+            foreach (BlobItem item in blobContainerClient.GetBlobs())
+            {
+                filelist.Add(item.Name);
+            }
+
+            return View(filelist);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    string blobName = formFile.FileName;
+
+                    string blobstorageconnectionstring = _configuration.GetConnectionString("blobconn");
+                    
+                    BlobContainerClient blobContainerClient = new BlobContainerClient(blobstorageconnectionstring, "photos");
+                    
+                    BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+                    
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+
+                    await blobClient.UploadAsync(filePath);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> DownloadFile(string fileName)
+        {
+            string downloadPath = @"D:\AzureAssignment\DownloadedBlob\";
+
+            string blobstorageconnectionstring = _configuration.GetConnectionString("blobconn");
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(blobstorageconnectionstring, "photos");
+
+            BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+
+            await blobClient.DownloadToAsync(downloadPath + fileName);
+
+            return RedirectToAction("Index");
+        }
+       
+        public async Task<IActionResult> Delete(string fileName)
+        {
+            string blobstorageconnectionstring = _configuration.GetConnectionString("blobconn");
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(blobstorageconnectionstring, "photos");
+
+            BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+
+            await blobClient.DeleteIfExistsAsync(DeleteSnapshotsOption.None);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,24 @@
+Cosmos db Account
+    Database
+        container <======== depending upon model data will go inside container could be collation,table,graph
+
+two ways of working with COSMOS
+  serverless
+  throughput
+     in/out operation
+       Request Unit (RU) ===> Blended measure of CPU,RAM,Hard Disk etc.
+       Amount of System resource you are using for each Cosmos db operation
+       (inset update delete,display)
+
+       For reading 1 KB of data,you need 1RU
+
+       Provision throughput
+       Shared throughput
+       
+
+
+
+ clientID : 8f024685-da31-4712-ba83-7c7710b93577
+ tenantID : ef89f4d2-cbae-4666-8356-df560f12fac4
+ secret value : iZ08Q~6FUB9XpiO4AoRVjU610WANizU3aBoCRdg6
+ secret ID : 73934efc-192c-48dc-859f-80338ca7c5b6
